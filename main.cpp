fac06e4ec5e2728322942c1542fcd3e1e6977ed9
Phat <tlphat18@apcs.vn>
2019-11-24 20:05:29 +0700

    Refactor run method, fix lights

--- main.cpp
+++ main.cpp
@@ -13,14 +13,15 @@ int main()
 	Screen sc;
 	//sc.splashScreen();
 	std::mutex* mtx = new std::mutex;
-	Lane* tmp1 = new Lane(Direction::RIGHT, Type::BIRD, 15, Color::BLUE, 1, 1);
-	Lane* tmp2 = new Lane(Direction::LEFT, Type::CAR, 50, Color::BLUE, 6, 2);
-	std::thread t1(&Lane::run, tmp1, sc, mtx);
-	std::thread t2(&Lane::run, tmp2, sc, mtx);
-	t1.join();
-	t2.join();
-	delete tmp1;
-	delete tmp2;
+	Lane* tmp1 = new Lane(Direction::RIGHT, Type::BIRD, 1);
+	//Lane* tmp2 = new Lane(Direction::LEFT, Type::CAR, 50, Color::BLUE, 6, 2);
+	//std::thread t1(&Lane::run, tmp1, sc, mtx);
+	//std::thread t2(&Lane::run, tmp2, sc, mtx);
+	//t1.join();
+	//t2.join();
+	//delete tmp1;
+	//delete tmp2;
+	tmp1->run(sc, mtx);
 	delete mtx;
 	return 0;
 }
